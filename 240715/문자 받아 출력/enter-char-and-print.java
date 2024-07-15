4b67f2962687a710f2e3b56843e87ce2c85d96c6
KNG9824 <aktlwjd98@gmail.com>
2024-07-15 12:15:47 +0900

    Title: 문자 받아 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240715/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc=new Scanner(System.in);
+        char c;
+        c=sc.next().charAt();
+        System.out.println(c);
+    }
+}
