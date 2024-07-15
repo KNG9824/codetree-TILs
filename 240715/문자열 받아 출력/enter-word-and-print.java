dc11bbcbcc747a8c6f6ea3efd9ff707bf5358a22
KNG9824 <aktlwjd98@gmail.com>
2024-07-15 12:17:24 +0900

    Title: 문자열 받아 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240715/문자열 받아 출력/enter-word-and-print.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc=new Scanner(System.in);
+        String s=next();
+        System.out.println(s);
+    }
+}
