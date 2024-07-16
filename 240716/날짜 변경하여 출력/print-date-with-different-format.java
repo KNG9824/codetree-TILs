c7ea57bdfbe81f97178f39c4f18007e5c554fbee
KNG9824 <aktlwjd98@gmail.com>
2024-07-16 09:59:45 +0900

    Title: 날짜 변경하여 출력, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 240716/날짜 변경하여 출력/print-date-with-different-format.java
+++ 240716/날짜 변경하여 출력/print-date-with-different-format.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
     Scanner sc=new Scanner(System.in);
-    sc.useDelimiter(".");
+    sc.useDelimiter("\\.");
 
 
     int yyyy=sc.nextInt();
