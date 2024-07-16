25bb8d05fc36e02fd79fac3bd4ecd4d5d26d1bf2
KNG9824 <aktlwjd98@gmail.com>
2024-07-16 10:03:19 +0900

    Title: 전화번호 바꾸기, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 240716/전화번호 바꾸기/change-phone-number.java
+++ 240716/전화번호 바꾸기/change-phone-number.java
@@ -4,8 +4,9 @@ public class Main {
         // 여기에 코드를 작성해주세요.
     Scanner sc=new Scanner(System.in);
     sc.useDelimiter("-");
+    int c=sc.nextInt();
     int a=sc.nextInt();
     int b=sc.nextInt();
-    System.out.println("010-"+b+"-"+a);
+    System.out.println("0"+c+"-"+b+"-"+a);
     }
 }
