674f2afb12cd92ad463806f963257fab7e88811f
KNG9824 <aktlwjd98@gmail.com>
2024-07-16 09:56:10 +0900

    Title: 입력받아 출력 2, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240716/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    sc.useDelimiter("-");
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    System.out.println(a+""+b);
+    }
+}
