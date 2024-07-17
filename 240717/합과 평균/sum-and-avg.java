d4be2ed655470629cc46243715c43e3c1d6f91ac
KNG9824 <aktlwjd98@gmail.com>
2024-07-17 10:00:09 +0900

    Title: 합과 평균, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240717/합과 평균/sum-and-avg.java
+++ 240717/합과 평균/sum-and-avg.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        Scannser sc=new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
-        double sum=0, avg;
+        int sum=0;
+        double avg;
         sum=a+b;
-        avg=sum/2;
-        System.out.printf("%d %.1f"sum,avg);
+        avg=sum/2.0;
+        System.out.printf("%d %.1f",sum,avg);
     }
 }
