f27bf04bcb15a229ae42a2cf89f70d580ee033a9
KNG9824 <aktlwjd98@gmail.com>
2024-07-17 09:58:22 +0900

    Title: 합과 평균, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240717/합과 평균/sum-and-avg.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scannser sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        double sum=0, avg;
+        sum=a+b;
+        avg=sum/2;
+        System.out.printf("%d %.1f"sum,avg);
+    }
+}
