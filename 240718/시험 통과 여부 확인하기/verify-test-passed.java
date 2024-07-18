3633344083a4ba9d909cf80629072d464fc01c24
KNG9824 <aktlwjd98@gmail.com>
2024-07-18 09:55:47 +0900

    Title: 시험 통과 여부 확인하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240718/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int p;
+    if(n>=80)
+        Systemout.println("pass");
+        else 
+        System.out.println((80-n)+" moer score");
+
+    }
+}
