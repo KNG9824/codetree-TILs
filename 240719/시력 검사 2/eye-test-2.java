34dec4db7431adf3c569572fbd77fcb50144f20f
KNG9824 <aktlwjd98@gmail.com>
2024-07-19 09:31:29 +0900

    Title: 시력 검사 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240719/시력 검사 2/eye-test-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scnnaer(System.in);
+    double a=sc.nextDouble();
+    if(a>1.0)
+    System.out.println("High");
+    else if(a<0.5)
+    System.out.println("Low");
+    else
+    System.out.println("Middle");
+    }
+}
