6122e3d2529900c20b48869e1dd7bd91fcdd0ecc
KNG9824 <aktlwjd98@gmail.com>
2024-07-19 09:26:45 +0900

    Title: 삼항연산자, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240719/삼항연산자/ternary-operator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    if(a==100)
+    System.out.println("pass");
+    else
+    System.out.println("failure");
+    }
+}
