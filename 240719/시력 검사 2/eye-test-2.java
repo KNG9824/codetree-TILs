8c499e1f20dac891838ec7f153a407544de7d475
KNG9824 <aktlwjd98@gmail.com>
2024-07-19 09:31:46 +0900

    Title: 시력 검사 2, Time: 132ms, Memory: 11MB, Status: Wrong Answer - CodeTree

--- 240719/시력 검사 2/eye-test-2.java
+++ 240719/시력 검사 2/eye-test-2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-    Scanner sc=new Scnnaer(System.in);
+    Scanner sc=new Scanner(System.in);
     double a=sc.nextDouble();
     if(a>1.0)
     System.out.println("High");
