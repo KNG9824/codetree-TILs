0adf81d15c85c0b92b13202bfa2bb340381700e5
KNG9824 <aktlwjd98@gmail.com>
2024-07-20 11:42:58 +0900

    Title: 최대 2번의 연산, Time: 153ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240720/최대 2번의 연산/up-to-2-calculations.java
+++ 240720/최대 2번의 연산/up-to-2-calculations.java
@@ -4,13 +4,14 @@ public class Main {
         // 여기에 코드를 작성해주세요.
     Scanner sc=new Scanner(System.in);
     int a=sc.nextInt();
-    if(a%2==0){
+    if(a%2==0)
         a/=2;
-        if(a%2!=0){
-            a=(a+1)/2;
+    else
+        a=(a+1)/2;
+    
+    
         
-        }
-    }
+    
     System.out.println(a);
     }
 }
