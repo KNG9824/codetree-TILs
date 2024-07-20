9324fc35646601d08c3200abfb4033a7c3dc44d7
KNG9824 <aktlwjd98@gmail.com>
2024-07-20 11:37:19 +0900

    Title: 살 수 있는 물건 2, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240720/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+
+    switch (a/500){
+    case 6: System.out.println("book"); break;
+    case 5: 
+    case 4:
+    case 3:
+    case 2:System.out.println("mask"); break;
+    case 1: System.out.println("pen"); break;
+    default: System.out.println("no");
+    }
+    }
+}
