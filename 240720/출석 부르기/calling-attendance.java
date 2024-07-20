89fffdddd751e4dad3fcb237d8bae309430eaefb
KNG9824 <aktlwjd98@gmail.com>
2024-07-20 11:27:40 +0900

    Title: 출석 부르기, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240720/출석 부르기/calling-attendance.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    if(a==1)
+        System.out.println("John");
+        else if(a==2)
+        System.out.println("Tom");
+        else if(a==3)
+        System.out.println("Paul");
+        else
+        System.out.println("Vacancy");
+    }
+}
