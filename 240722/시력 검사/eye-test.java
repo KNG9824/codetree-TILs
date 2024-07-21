9b015f79d59a90c484e779089f25ac00614431cf
KNG9824 <aktlwjd98@gmail.com>
2024-07-22 07:32:14 +0900

    Title: 시력 검사, Time: 144ms, Memory: 11MB, Status: Passed - CodeTree

--- 240722/시력 검사/eye-test.java
+++ 240722/시력 검사/eye-test.java
@@ -8,7 +8,7 @@ public class Main {
 
     if(a>=1.0&&b>=1.0)
         System.out.println("High");
-    else if(a>=0.5&&a<1.0&&b<1.0&&b>=0.5)
+    else if(a>=0.5&&b>=0.5)
         System.out.println("Middle");
         else if(a<0.5||b<0.5)
         System.out.println("Low");
