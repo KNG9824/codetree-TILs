8481d5c04c7c3c6598759514f62a79e361ff7ae8
KNG9824 <aktlwjd98@gmail.com>
2024-07-21 11:34:50 +0900

    Title: 두 숫자의 짝홀 여부, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240721/두 숫자의 짝홀 여부/parity-of-two-numbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    if(a%2==0)
+        System.out.println("even");
+    else
+        System.out.println("odd");
+
+    if(b%2==0)
+        System.out.println("even");
+    else
+        System.out.println("odd");
+
+
+    }
+}
