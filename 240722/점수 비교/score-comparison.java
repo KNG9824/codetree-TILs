485e3ce84cf391e9f860ca587f7733af4f76e64f
KNG9824 <aktlwjd98@gmail.com>
2024-07-22 07:25:16 +0900

    Title: 점수 비교, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240722/점수 비교/score-comparison.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c=sc.nextInt();
+    int d=sc.nextInt();
+
+    if(a>c&&b>d)
+        System.out.println("1");
+        else
+        System.out.println("0");
+    }
+}
