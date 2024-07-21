a0413841e0822567b8804947767f96d3c0815753
KNG9824 <aktlwjd98@gmail.com>
2024-07-21 17:22:22 +0900

    Title: 4번의 크기 비교, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240721/4번의 크기 비교/4-time-comparison.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+int d=sc.nextInt();
+int e=sc.nextInt();
+if(a>b)
+        System.out.println("1");
+    else 
+        System.out.println("0");
+        if(a>c)
+        System.out.println("1");
+    else 
+        System.out.println("0");
+        if(a>d)
+        System.out.println("1");
+    else 
+        System.out.println("0");
+        if(a>e)
+        System.out.println("1");
+    else 
+        System.out.println("0");
+    }
+}
