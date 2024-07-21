1e157f3eeb273381505ddd85e3b9020fd08fb442
KNG9824 <aktlwjd98@gmail.com>
2024-07-22 07:44:33 +0900

    Title: 특정 조건 세 정수 비교, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240722/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c=sc.nextInt();
+
+    int min=0;
+    if(a<b){
+        if(a<c)
+        min=a;
+        else
+        min=c;
+        }
+        else{
+            if(b<c)
+            min=b;
+            else
+            min=c;
+        }
+        if(a==min)
+            System.out.print("1")
+            else
+            System.out.print("0");
+        if(a==b==c)
+        System.out.print("1")
+            else
+            System.out.print("0");
+    
+
+    }
+}
