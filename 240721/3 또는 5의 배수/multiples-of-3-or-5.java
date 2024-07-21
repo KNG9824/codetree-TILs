addfd3ddcc11acabf25f573604adb26b3e064a57
KNG9824 <aktlwjd98@gmail.com>
2024-07-21 11:38:11 +0900

    Title: 3 또는 5의 배수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240721/3 또는 5의 배수/multiples-of-3-or-5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    if(a%3==0)
+        system.out.println("Yes");
+        else
+        system.out.println("No");
+
+    if(a%5==0)
+        system.out.println("Yes");
+        else
+        system.out.println("No");
+
+    
+    }
+}
