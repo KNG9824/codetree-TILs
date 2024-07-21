11ff54444c798c49039d57b453b621afa43eafa2
KNG9824 <aktlwjd98@gmail.com>
2024-07-21 17:19:52 +0900

    Title: 비교 연산, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240721/비교 연산/comparison-operator.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    if(a>=b)
+        System.out.println("1");
+    else 
+        System.out.println("0");
+
+    if(a>b)
+        System.out.println("1");
+    else 
+        System.out.println("0");
+
+    if(a<=b)
+        System.out.println("1");
+    else 
+        System.out.println("0");
+
+    if(a<b)
+        System.out.println("1");
+    else 
+        System.out.println("0");
+
+    if(a==b)
+        System.out.println("1");
+    else 
+        System.out.println("0");
+
+    if(a!=b)
+        System.out.println("1");
+    else 
+        System.out.println("0");
+    }
+}
