3c4a5dfa93c112c1701eedc22cf943564e3c8380
KNG9824 <aktlwjd98@gmail.com>
2024-07-23 08:11:11 +0900

    Title: 굉장한 숫자, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 240723/굉장한 숫자/amazing-number.java
+++ 240723/굉장한 숫자/amazing-number.java
@@ -4,7 +4,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
     Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
-    if((a%2!=0&&a%3==0)||(a%2==0&&a%5==0))
+    if((n%2!=0&&n%3==0)||(n%2==0&&n%5==0))
     System.out.println("true");
     else
     System.out.println("false");
