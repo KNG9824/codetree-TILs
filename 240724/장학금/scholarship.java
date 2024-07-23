3b8ea56bd5c176080db918ded37fa6569452ce59
KNG9824 <aktlwjd98@gmail.com>
2024-07-24 08:55:02 +0900

    Title: 장학금, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240724/장학금/scholarship.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int mid=sc.nextInt();
+    int fin=sc.nextInt();
+    if(mid>=90&&fin>=95)
+        System.out.println("100000");
+        else if(mid>=90&&fin>=90&&fin<95)
+        System.out.println("50000");
+        else
+        System.out.println("0");
+    }
+}
