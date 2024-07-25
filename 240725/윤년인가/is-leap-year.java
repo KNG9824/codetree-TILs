fe63b958afa4a20af2d7b7f281e57f0eb817403c
KNG9824 <aktlwjd98@gmail.com>
2024-07-25 16:17:48 +0900

    Title: 윤년인가, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240725/윤년인가/is-leap-year.java
+++ 240725/윤년인가/is-leap-year.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
     Scanner sc=new Scanner(System.in);
-    int y=sc.nxtInt();
+    int y=sc.nextInt();
     if(y%4==0){
         if(y%100==0&&y%400!=0)
             System.out.println("false");
@@ -11,6 +11,6 @@ public class Main {
             System.out.println("true");
     }
     else
-    System.out,..println("false");
+    System.out.println("false");
     }
 }
