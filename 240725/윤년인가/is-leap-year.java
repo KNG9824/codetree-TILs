a7227e2b3f7b0f382e78d5aee1ef1802199521c1
KNG9824 <aktlwjd98@gmail.com>
2024-07-25 16:17:07 +0900

    Title: 윤년인가, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240725/윤년인가/is-leap-year.java
+++ 240725/윤년인가/is-leap-year.java
@@ -4,11 +4,12 @@ public class Main {
         // 여기에 코드를 작성해주세요.
     Scanner sc=new Scanner(System.in);
     int y=sc.nxtInt();
-    if(y%4==0)
+    if(y%4==0){
         if(y%100==0&&y%400!=0)
             System.out.println("false");
             else
             System.out.println("true");
+    }
     else
     System.out,..println("false");
     }
