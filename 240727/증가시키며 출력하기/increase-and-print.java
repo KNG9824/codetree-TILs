8a44768af71a1da4aebd44f2fb8e9a0530034035
KNG9824 <aktlwjd98@gmail.com>
2024-07-27 08:51:51 +0900

    Title: 증가시키며 출력하기, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

--- 240727/증가시키며 출력하기/increase-and-print.java
+++ 240727/증가시키며 출력하기/increase-and-print.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        for(int i=5;i<18;i+2)
+        for(int i=5;i<18;i+=2)
             System.out.print(i+" ");
     }
 }
