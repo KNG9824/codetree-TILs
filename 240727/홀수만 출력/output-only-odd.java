56002ec654684aa4fcbf9e7dd10e7530e0a555d9
KNG9824 <aktlwjd98@gmail.com>
2024-07-27 08:57:24 +0900

    Title: 홀수만 출력, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240727/홀수만 출력/output-only-odd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    for(int i=a;i<=b;i++)
+    {if(i%2==1)
+    System.out.print(i+" ");
+
+    }
+    }
+}
