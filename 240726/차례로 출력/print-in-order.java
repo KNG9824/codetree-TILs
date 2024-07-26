3fe964f3f99634f7e9d8822770442d214b5b3aae
KNG9824 <aktlwjd98@gmail.com>
2024-07-26 12:25:09 +0900

    Title: 차례로 출력, Time: 104ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240726/차례로 출력/print-in-order.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        for(int i=5;i<18;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
