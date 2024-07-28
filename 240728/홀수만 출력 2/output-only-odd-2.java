c3f0e334e5075488cb2a14e415417ad5f7612395
KNG9824 <aktlwjd98@gmail.com>
2024-07-28 17:19:31 +0900

    Title: 홀수만 출력 2, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240728/홀수만 출력 2/output-only-odd-2.java
+++ 240728/홀수만 출력 2/output-only-odd-2.java
@@ -1,13 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-    Scanner sc=new Scanner(System.in);
-    int a=sc.nextInt();
-    int b=sc.nextInt();
-    for(int i=b;i>=a;i--){
-        if(i%2!=0)
-            System.out.print(i+" ");
-    }
+        // Scanner를 사용하여 사용자로부터 입력받기
+        Scanner sc = new Scanner(System.in);
+        
+        // 두 정수 a와 b를 입력받기
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // a와 b의 크기에 따라 반복문을 설정하여 홀수를 내림차순으로 출력
+        
+            for (int i = a; i >= b; i--) {
+                if (i % 2 != 0) {
+                    System.out.print(i + " ");
+                }
+            }
+        
     }
 }
