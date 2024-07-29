8f8d833f2b32a4a63371251a62a77e7d1427dea7
KNG9824 <aktlwjd98@gmail.com>
2024-07-29 09:29:00 +0900

    Title: 배열 채우고 출력하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240729/배열 채우고 출력하기/filling-array-and-print.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc=new Scanner(System.in);
+        char[] a=new char[10];
+        for(int i=0;i<10;i++)
+            a[i]=sc.next().charAt(0);
+        
+        for(int i=9;i>=0;i--)
+            System.out.print(a[i]);
+        }
+}
