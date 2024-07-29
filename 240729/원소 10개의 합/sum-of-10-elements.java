ace89da54077e9be6adae2b37a36674a95634f47
KNG9824 <aktlwjd98@gmail.com>
2024-07-29 09:26:28 +0900

    Title: 원소 10개의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240729/원소 10개의 합/sum-of-10-elements.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scnnaer(System.in);
+    int[] a=new int[10];
+    for(int i=0;i<10;i++)
+        a[i]=sc.nextInt();
+
+        sum=0;
+        for(int i=0;i<10;i++)
+            sum+=a[i];
+
+            System.out.print(sum);
+        }
+}
