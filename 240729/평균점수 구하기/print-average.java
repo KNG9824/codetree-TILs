3497fe2c12983b2c28c809041104da3a897e439b
KNG9824 <aktlwjd98@gmail.com>
2024-07-29 09:47:35 +0900

    Title: 평균점수 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240729/평균점수 구하기/print-average.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    double score=new double[8];
+    double sum=0;
+    for(int i=0;i<8;i++){
+        score[i]=sc.nextDouble();
+        sum+=score[i];
+    }
+    double avg=sum/8;
+    System.out.printf("%.1f",avg);
+ }
+}
