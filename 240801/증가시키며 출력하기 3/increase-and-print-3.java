6c0d87dbce5627efaa928351e6da07a75f86e913
KNG9824 <aktlwjd98@gmail.com>
2024-08-01 07:53:09 +0900

    Title: 증가시키며 출력하기 3, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

--- 240801/증가시키며 출력하기 3/increase-and-print-3.java
+++ 240801/증가시키며 출력하기 3/increase-and-print-3.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
     int i=10;
-    whiln(i<=26){
+    while(i<=26){
         System.out.print(i+" ");
         i+=2;
     }
