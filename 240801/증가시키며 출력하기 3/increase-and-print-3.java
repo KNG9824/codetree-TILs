5998b25aacf1b4d68d9c198c4dba2f6d1b43c657
KNG9824 <aktlwjd98@gmail.com>
2024-08-01 07:52:50 +0900

    Title: 증가시키며 출력하기 3, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240801/증가시키며 출력하기 3/increase-and-print-3.java
+++ 240801/증가시키며 출력하기 3/increase-and-print-3.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-    int =10;
+    int i=10;
     whiln(i<=26){
         System.out.print(i+" ");
         i+=2;
