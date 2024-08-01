4ccde088c1abe4e2ff6a7fda8513d8ca6f1aa5d1
KNG9824 <aktlwjd98@gmail.com>
2024-08-02 08:35:02 +0900

    Title: a/b 출력, Time: 136ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240802/a/b 출력/a-divide-b.java
+++ 240802/a/b 출력/a-divide-b.java
@@ -3,19 +3,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-       
         int a = sc.nextInt();
-        
         int b = sc.nextInt();
 
-        // Perform the division and store the result as a string
-        String result = String.format("%.21f", (double) a / b);
+        // Perform the division
+        double result = (double) a / b;
+
+        // Convert the result to a string with sufficient precision
+        String resultStr = String.format("%.22f", result);
 
         // Find the position of the 21st decimal place and truncate the result
-        int dotIndex = result.indexOf('.');
-        String truncatedResult = result.substring(0, dotIndex + 21); // 21 decimal places + 1 for the dot
+        int dotIndex = resultStr.indexOf('.');
+
+        // We need to check the 22nd decimal place to decide if we need to round down
+        if (resultStr.charAt(dotIndex + 22) >= '5') {
+            // Perform rounding down manually
+            resultStr = resultStr.substring(0, dotIndex + 21) + (char) (resultStr.charAt(dotIndex + 21) + 1);
+        } else {
+            resultStr = resultStr.substring(0, dotIndex + 22);
+        }
+
+        // Remove trailing zeros and the dot if necessary
+        resultStr = resultStr.replaceAll("0*$", "").replaceAll("\\.$", "");
 
         // Print the result
-        System.out.println(truncatedResult);
+        System.out.println(resultStr);
     }
 }
