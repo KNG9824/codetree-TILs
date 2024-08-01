cd87c51863df3956e281b1c1f9e9a9fec36e12c6
KNG9824 <aktlwjd98@gmail.com>
2024-08-02 08:27:26 +0900

    Title: 문자 출력하기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240802/문자 출력하기/print-text.java
+++ 240802/문자 출력하기/print-text.java
@@ -5,6 +5,6 @@ public class Main {
     Scanner sc=new Scanner(System.in);
     char c=sc.next().charAt(0);
     for(int i=0;i<8;i++)
-        System.out.pirnt(c);
+        System.out.print(c);
     }
 }
