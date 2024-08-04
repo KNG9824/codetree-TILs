1af0823bfe9a25f9d9c9440c230d0783f1d9956b
KNG9824 <aktlwjd98@gmail.com>
2024-08-04 09:43:45 +0900

    Title: 369 게임, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240804/369 게임/369-game.java
@@ -0,0 +1,15 @@
+import java.util*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    for(int i=0;i<=n;i++){
+        if(i%3==0||(i/10)%3==0){
+            System.out.print("0");
+        }
+        else
+        System.out.print(i+" ");
+    }
+    }
+}
