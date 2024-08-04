f8938defb577f56b36b63a496d2aa48258219abb
KNG9824 <aktlwjd98@gmail.com>
2024-08-04 09:49:51 +0900

    Title: 369 게임, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240804/369 게임/369-game.java
+++ 240804/369 게임/369-game.java
@@ -1,15 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-    Scanner sc=new Scanner(System.in);
-    int n=sc.nextInt();
-    for(int i=1;i<=n;i++){
-        if(i%3==0||(i/10!=0&&(i%10)%3==0)){
-            System.out.print("0 ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || contains369(i)) {
+                System.out.print("0 ");
+            } else {
+                System.out.print(i + " ");
+            }
         }
-        else
-        System.out.print(i+" ");
     }
+
+    public static boolean contains369(int number) {
+        String str = String.valueOf(number);
+        return str.contains("3") || str.contains("6") || str.contains("9");
     }
 }
