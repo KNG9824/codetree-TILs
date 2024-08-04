2c9d8a62c1b72b14cbf913a0872a61122ba0122e
KNG9824 <aktlwjd98@gmail.com>
2024-08-04 09:44:47 +0900

    Title: 369 게임, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240804/369 게임/369-game.java
+++ 240804/369 게임/369-game.java
@@ -4,7 +4,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
     Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
-    for(int i=0;i<=n;i++){
+    for(int i=1;i<=n;i++){
         if(i%3==0||((i+10)/10)%3==0){
             System.out.print("0 ");
         }
