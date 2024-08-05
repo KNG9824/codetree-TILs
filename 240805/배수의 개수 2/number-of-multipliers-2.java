0502365373fa6d231b5e58310f50c2d4a4b81c80
KNG9824 <aktlwjd98@gmail.com>
2024-08-05 09:22:31 +0900

    Title: 배수의 개수 2, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240805/배수의 개수 2/number-of-multipliers-2.java
+++ 240805/배수의 개수 2/number-of-multipliers-2.java
@@ -4,7 +4,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
     Scanner sc=new Scanner(System.in);
     int[] array=new int[10];
-    it count=0;
+    int count=0;
     for(int i=0;i<10;i++){
         array[i]=sc.nextInt();
         if(array[i]%2!=0)
