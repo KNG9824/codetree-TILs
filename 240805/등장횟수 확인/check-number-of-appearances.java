1f6b7b3ef0a7d87470ecebc1471bdb748c1e6f06
KNG9824 <aktlwjd98@gmail.com>
2024-08-05 10:20:25 +0900

    Title: 등장횟수 확인, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240805/등장횟수 확인/check-number-of-appearances.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int[] a=new int[5];
+    int cnt=0;
+    for(int i=0;i<5;i++){
+        a[i]=sc.nextInt()
+        if(a[i]%2==0)
+        cnt++;
+    }
+    System.out.print(cnt);
+        }
+}
