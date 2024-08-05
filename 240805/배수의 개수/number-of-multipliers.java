ada66019fca2fe9824a4234c57c4c02f29b4c739
KNG9824 <aktlwjd98@gmail.com>
2024-08-05 10:05:30 +0900

    Title: 배수의 개수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240805/배수의 개수/number-of-multipliers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int[] array=new int[10];
+    int cnt=0,cnt2=0;
+    for(int i=0;i<10;i++){
+        array[i]=sc.nextInt();
+        if(array[i]%3==0)
+        cnt1++;
+        if(array[i]%5==0)
+        cnt2++;
+    }
+    System.out.print(cnt1+" "+cnt2);
+
+    }
+}
