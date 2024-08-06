89b6b8f62241ba6c6a607fec324be9de61b80069
KNG9824 <aktlwjd98@gmail.com>
2024-08-06 11:42:23 +0900

    Title: a부터 b까지의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240806/a부터 b까지의 합/sum-from-a-to-b.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int sum=0;
+    for(int i=a;i<=b;i++)
+        sum+=i;
+        System.out.print(i);
+            }
+}
