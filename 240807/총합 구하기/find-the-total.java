8658e5435b5f1e8282cf2ae7e2345bd3eb1289ed
KNG9824 <aktlwjd98@gmail.com>
2024-08-07 08:40:40 +0900

    Title: 총합 구하기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240807/총합 구하기/find-the-total.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int sum=0;
+    for(int i=a;i<=b;i++){
+        if(i%6==0&&i%8!=0)
+        sum+=i;
+    }
+    System.out.println(sum);
+    }
+}
