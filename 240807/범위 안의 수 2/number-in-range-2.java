e83912636253900f08428428543d515384027ddd
KNG9824 <aktlwjd98@gmail.com>
2024-08-07 08:32:14 +0900

    Title: 범위 안의 수 2, Time: 154ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240807/범위 안의 수 2/number-in-range-2.java
+++ 240807/범위 안의 수 2/number-in-range-2.java
@@ -5,11 +5,14 @@ public class Main {
     Scanner sc=new Scanner(System.in);
     int[] a=new int[10];
     double sum=0, avg;
+    int cnt=0;
     for(int i=0;i<10;i++){
     a[i]=sc.nextInt();
+    if(a[i]>0&&a[i]<=200){
     sum+=a[i];
+    cnt++;}
     }
-    avg=sum/10;
+    avg=sum/cnt;
     System.out.print((int)sum+" "+avg);
     }
 }
