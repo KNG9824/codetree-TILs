5dadc680b37393322feb88d12b432c1e5066cfaa
KNG9824 <aktlwjd98@gmail.com>
2024-08-08 11:26:03 +0900

    Title: a의 b승, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240808/a의 b승/a-to-the-power-of-b.java
+++ 240808/a의 b승/a-to-the-power-of-b.java
@@ -4,9 +4,9 @@ public class Main {
     Scanner sc=new Scanner(System.in);
     int a=sc.nextInt();
     int b=sc.nextInt();
-    int p=0;
+    
     int c=a;
-    for(int i=0;i<=b;i++){
+    for(int i=0;i<b;i++){
     c=a*c;
     }
         System.out.println(c);
