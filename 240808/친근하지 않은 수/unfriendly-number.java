55139869abde173b54e8b4308888665566537658
KNG9824 <aktlwjd98@gmail.com>
2024-08-08 11:35:15 +0900

    Title: 친근하지 않은 수, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240808/친근하지 않은 수/unfriendly-number.java
+++ 240808/친근하지 않은 수/unfriendly-number.java
@@ -4,7 +4,7 @@ public class Main {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int cnt=0;
-        for(int i=1;i<n;i++){
+        for(int i=1;i<=n;i++){
             if(i%2==0||i%3==0||i%5==0)
             continue;
             cnt++;
