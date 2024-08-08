430a7e5bb4db4747440f9ed1444a07073898ea92
KNG9824 <aktlwjd98@gmail.com>
2024-08-09 08:10:43 +0900

    Title: 0 나올때까지 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240809/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int n;
+    while(true){
+        n=sc.nextInt();
+        System.out.println(n);
+        if(n==0)
+        break;
+    }
+    }
+}
