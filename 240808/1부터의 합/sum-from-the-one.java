0842fb87954f18e26a2198db22d7646d7b9d3269
KNG9824 <aktlwjd98@gmail.com>
2024-08-08 21:28:12 +0900

    Title: 1부터의 합, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240808/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int sum=0;
+    for(int i=1;i<n;i++){
+        sum+=i;
+        if(sum>n)
+        break;
+    }
+    System.out.print(sum);
+    }
+}
