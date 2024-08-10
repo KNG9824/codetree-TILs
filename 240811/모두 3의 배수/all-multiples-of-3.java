ab7cce037f50bd517aecb525969444df38fe3835
KNG9824 <aktlwjd98@gmail.com>
2024-08-11 08:56:19 +0900

    Title: 모두 3의 배수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240811/모두 3의 배수/all-multiples-of-3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int[] a=new int[5];
+    int cnt=0;
+    for(int i=0;i<5;i++){
+        a[i]=sc.nextInt();
+    }
+    for(int i=0;i<5;i++){
+        if(a[i]%3==0)
+        cnt++
+    }
+    if(cnt==5)
+    System.out.print("1");
+    else
+    System.out.print("0");
+    }
+}
