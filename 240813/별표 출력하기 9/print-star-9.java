bcbf17445607a45c628bf201a77a0c90e5641cdf
KNG9824 <aktlwjd98@gmail.com>
2024-08-13 09:20:49 +0900

    Title: 별표 출력하기 9, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240813/별표 출력하기 9/print-star-9.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        // 첫 번째 줄부터 n번째 줄까지 패턴을 출력
+        for (int i = 0; i < n; i++) {
+            // 앞에 공백 출력
+            for (int j = 0; j < n-i-1; j++) {
+                System.out.print("  ");
+            }
+            // 별 출력
+            for (int k = 0; k < 2*i+1; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
