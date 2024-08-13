31992cd914965beafbdded558ca08900b8033338
KNG9824 <aktlwjd98@gmail.com>
2024-08-13 09:03:56 +0900

    Title: 별표 출력하기 4, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240813/별표 출력하기 4/print-star-4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++)
+        System.out.print("* ");
+        System.out.println("");
+    }
+    for(int i=1;i<n;i++){
+        for(int j=0;j<i+1;j++)
+        System.out.print("* ");
+        System.out.println("");
+    }
+    }
+}
