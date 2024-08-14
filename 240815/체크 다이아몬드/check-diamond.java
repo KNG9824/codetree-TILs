5b21b96d019653d51e8e638022f84452b698d72b
KNG9824 <aktlwjd98@gmail.com>
2024-08-15 07:18:23 +0900

    Title: 체크 다이아몬드, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240815/체크 다이아몬드/check-diamond.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    for(int i=0;i<n;i++){
+        for(int p=0;p<n-i-1;p++)
+        System.out.print(" ");
+        for(int j=0;j<i+1;j++)
+        System.out.print("* ");
+        System.out.println("");
+    }
+    for(int i=1;i<n;i++){
+        for(int p=0;p<i;p++)
+        System.out.print(" ");
+        for(int j=0;j<n-i;j++)
+        System.out.print("* ");
+        System.out.println("");
+    }
+    }
+}
