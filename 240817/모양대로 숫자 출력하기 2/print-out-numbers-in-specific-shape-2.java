042e019da13e0662c4510ba650a856f8ce187f2b
KNG9824 <aktlwjd98@gmail.com>
2024-08-17 08:32:04 +0900

    Title: 모양대로 숫자 출력하기 2, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 240817/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.java
+++ 240817/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.java
@@ -7,7 +7,7 @@ public class Main {
         int cnt=2;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                System.out.print(cnt%10);
+                System.out.print((cnt%10)+" ");
                 if(cnt%10==8)
                 cnt+=4;
                 else
