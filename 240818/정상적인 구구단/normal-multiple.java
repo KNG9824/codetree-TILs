cbdb75346bf5fdba9ee50f642855cea8f9df54ce
KNG9824 <aktlwjd98@gmail.com>
2024-08-18 12:07:03 +0900

    Title: 정상적인 구구단, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240818/정상적인 구구단/normal-multiple.java
+++ 240818/정상적인 구구단/normal-multiple.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-    Scanner sc=new Scaaner(System.in);
+    Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++)
