8856ee9c4c511f88fe5e64695914502ecd3e6b20
KNG9824 <aktlwjd98@gmail.com>
2024-08-18 11:51:49 +0900

    Title: 좌우로 반복하며 출력, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240818/좌우로 반복하며 출력/print-in-left-right-reverse.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    for(int i=0;i<n;i++){
+        if(i%2==0){
+        for(int j=1;j<=n;j++){
+            System.out.print((j+1)+" ");
+        }
+    }
+        else{
+            for(int j=n;j>0;j--)
+            System.out.print(j+" ");
+        }
+    System.out.println("");
+    }
+}
+}
