4ae50ee8afaa89e47bf0321a0fc6f12d73843913
KNG9824 <aktlwjd98@gmail.com>
2024-08-18 11:52:30 +0900

    Title: 좌우로 반복하며 출력, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240818/좌우로 반복하며 출력/print-in-left-right-reverse.java
+++ 240818/좌우로 반복하며 출력/print-in-left-right-reverse.java
@@ -7,12 +7,12 @@ public class Main {
     for(int i=0;i<n;i++){
         if(i%2==0){
         for(int j=1;j<=n;j++){
-            System.out.print(j+" ");
+            System.out.print(j);
         }
     }
         else{
             for(int j=n;j>0;j--)
-            System.out.print(j+" ");
+            System.out.print(j);
         }
     System.out.println("");
     }
