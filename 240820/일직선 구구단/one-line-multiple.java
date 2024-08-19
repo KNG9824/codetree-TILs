be89d8290072b6f4f3d353b3f3d10ec9a253be34
KNG9824 <aktlwjd98@gmail.com>
2024-08-20 08:21:58 +0900

    Title: 일직선 구구단, Time: 206ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240820/일직선 구구단/one-line-multiple.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++)
+        System.out.println(i+" * "+j+" = "+i*j);
+    }
+    }
+}
