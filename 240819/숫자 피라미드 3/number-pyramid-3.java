b52fe60790a036a498fe4124211c4f3c4f16b630
KNG9824 <aktlwjd98@gmail.com>
2024-08-19 09:47:07 +0900

    Title: 숫자 피라미드 3, Time: 191ms, Memory: 11MB, Status: Passed - CodeTree

--- 240819/숫자 피라미드 3/number-pyramid-3.java
+++ 240819/숫자 피라미드 3/number-pyramid-3.java
@@ -5,8 +5,8 @@ public class Main {
     Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
     for(int i=1;i<=n;i++){
-        for(int j=i;j<i+1;j++)
-        System.out.pirnt(j*i+" ");
+        for(int j=1;j<i+1;j++)
+        System.out.print(j*i+" ");
         System.out.println("");
     }
     }
