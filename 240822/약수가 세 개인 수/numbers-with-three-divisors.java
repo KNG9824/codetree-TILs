7f3ec6d998470451088d354a307ff4795739b5e1
KNG9824 <aktlwjd98@gmail.com>
2024-08-22 09:01:09 +0900

    Title: 약수가 세 개인 수, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240822/약수가 세 개인 수/numbers-with-three-divisors.java
+++ 240822/약수가 세 개인 수/numbers-with-three-divisors.java
@@ -8,7 +8,7 @@ public class Main {
     int c=0;
     for(int i=a;i<=b;i++){
         int cnt=0;
-        for(int j=1;j<=i/2;j++){
+        for(int j=1;j<=i;j++){
             if(i%j==0)
             cnt++;
         }
