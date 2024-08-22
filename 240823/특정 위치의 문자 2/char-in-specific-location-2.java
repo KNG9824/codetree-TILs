af6a9d98a8886814a4cfb528c40b582d0e6a9756
KNG9824 <aktlwjd98@gmail.com>
2024-08-23 08:48:49 +0900

    Title: 특정 위치의 문자 2, Time: 120ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240823/특정 위치의 문자 2/char-in-specific-location-2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int[] a=new int[10];
+    for(int i=0;i<10;i++){
+        a[i]=sc.next().charAt(0);
+        if(i==1||i==4||i==7)
+        System.out.print(a[i]+" ");
+    }
+    }
+}
