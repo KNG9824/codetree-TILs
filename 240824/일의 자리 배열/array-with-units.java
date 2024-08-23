bafc3e4d7b2301fcfba2aede0e1403b175820c4c
KNG9824 <aktlwjd98@gmail.com>
2024-08-24 08:57:39 +0900

    Title: 일의 자리 배열, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240824/일의 자리 배열/array-with-units.java
+++ 240824/일의 자리 배열/array-with-units.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
     Scanner sc=new Scanner(System.in);
-    int a=new int[10];
+    int[] a=new int[10];
     for(int i=0;i<2;i++)
     a[i]=sc.nextInt();
     for(int i=2;i<10;i++){
