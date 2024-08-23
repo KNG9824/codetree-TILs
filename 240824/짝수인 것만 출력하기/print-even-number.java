602c1b76acf30091ef0f49abfc8fa91c3f66b259
KNG9824 <aktlwjd98@gmail.com>
2024-08-24 08:58:46 +0900

    Title: 짝수인 것만 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240824/짝수인 것만 출력하기/print-even-number.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int[] a=new int[n];
+    for(int i=0;i<n;i++){
+    a[i]=sc.nextInt();
+    if(a[i]%2==0)
+    Sysem.out.pnt(a[i]+" ");
+    }
+
+
+    }
+}
