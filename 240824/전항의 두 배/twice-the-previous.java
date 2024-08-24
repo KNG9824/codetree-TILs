25a8114db0a0d41588211cb192778bcf00e79204
KNG9824 <aktlwjd98@gmail.com>
2024-08-24 09:09:10 +0900

    Title: 전항의 두 배, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240824/전항의 두 배/twice-the-previous.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int[] a=new int[10];
+    a[0]=sc.nextInt();
+    a[1]=sc.nextInt();
+    for(int i=2;i<10;i++){
+        a[i]=a[i-1]+2*a[i-2];
+    }
+    for(int i=0;i<10;i++)
+    System.out.print(a[i]+" ");
+    }
+}
