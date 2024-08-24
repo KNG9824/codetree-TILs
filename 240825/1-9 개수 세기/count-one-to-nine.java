7ec9751347b1faf7142cbbc7989ed0f28494663d
KNG9824 <aktlwjd98@gmail.com>
2024-08-25 08:24:17 +0900

    Title: 1-9 개수 세기, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240825/1-9 개수 세기/count-one-to-nine.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int[] a=new int[n];
+    int[] c=new int[9];
+    for(int i=0;i<9;i++){
+        c[i]=0;
+    }
+    for(int i=0;i<n;i++){
+        a[i]=sc.nextInt();
+        switch(a[i]){
+            case 9: c[8]++; break;
+            case 8: c[7]++; break;
+            case 7: c[6]++; break;
+            case 6: c[5]++; break;
+            case 5: c[4]++; break;
+            case 4: c[3]++; break;
+            case 3: c[2]++; break;
+            case 2: c[1]++; break;
+            case 1: c[0]++; break;
+            
+        }
+
+    }
+    for(int i=0;i<9;i++)
+    System.out.println(c[i]);
+    }
+}
