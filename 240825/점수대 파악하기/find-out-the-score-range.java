08f4937a0bb67d1086498d7276038a733f4b6f35
KNG9824 <aktlwjd98@gmail.com>
2024-08-25 09:07:42 +0900

    Title: 점수대 파악하기, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240825/점수대 파악하기/find-out-the-score-range.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int[] c=new int[10];
+    for(int i=0;i<10;i++)
+    c[i]=0;
+    for(int i=0;i<101;i++){
+        int n=sc.nextInt();
+        if(n==0)
+        break;
+        switch(n/10){
+            case 10: c[9]++; break;
+            case 9: c[8]++; break;
+            case 8: c[7]++; break;
+            case 7: c[6]++; break;
+            case 6: c[5]++; break;
+            case 5: c[4]++; break;
+            case 4: c[3]++; break;
+            case 3: c[2]++; break;
+            case 2: c[1]++; break;
+            case 1: c[0]++; break;
+        }
+    }
+    for(int i=9;i>=0;i--)
+    System.out.println(((i+1)*10)+" - "+c[i]);
+    }
+}
