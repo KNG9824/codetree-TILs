de265086839048a151d30062834f6616df7dcc5f
KNG9824 <aktlwjd98@gmail.com>
2024-08-27 07:52:05 +0900

    Title: 가장 큰 수 고르기, Time: 113ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240827/가장 큰 수 고르기/picking-biggest-number.java
+++ 240827/가장 큰 수 고르기/picking-biggest-number.java
@@ -3,13 +3,16 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
     Scanner sc=new Scanner(System.in);
-    int max=0;
+    
     int[] a=new int[10];
     a[0]=sc.nextInt();
+    int max=a[0];
     for(int i=1;i<10;i++){
         a[i]=sc.nextInt();
         if(a[i]>a[i-1])
         max=a[i];
+        else
+        max=a[i-1];
     }
     System.out.print(max);
     }
