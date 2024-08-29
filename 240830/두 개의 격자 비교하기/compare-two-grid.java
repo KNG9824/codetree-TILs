47f90567a5f45b68ab64ff3d06b25b47c7ec28d0
KNG9824 <aktlwjd98@gmail.com>
2024-08-30 07:07:43 +0900

    Title: 두 개의 격자 비교하기, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240830/두 개의 격자 비교하기/compare-two-grid.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int m=sc.nextInt();
+    int[][] a=new int[n][m];
+    int[][] b=new int[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++)
+        a[i][j]=sc.nextInt();
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++)
+        b[i][j]=sc.nextInt();
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(a[i][j]==b[i][j])
+            System.out.print("1 ");
+            else
+            System.out.print("0 ");
+            System.out.println();
+        }
+        
+    }
+    }
+}
