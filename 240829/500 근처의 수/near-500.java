6466bb1fcdd83962848c42cd16ef7aaf3b1e1c26
KNG9824 <aktlwjd98@gmail.com>
2024-08-29 09:32:08 +0900

    Title: 500 근처의 수, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240829/500 근처의 수/near-500.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int[] a=new int[10];
+    for(int i=0;i<10;i++)
+    a[i]=sc.nextInt();
+    int max=0;
+    int max2=500;
+    for(int i=0;i<10;i++){
+        if(a[i]<500&&a[i]>max)
+        max=a[i];
+        if(a[i]>500&&a[i]>max2)
+        max2=a[i];
+    }
+    System.out.print(max+" "+max2);
+    }
+}
