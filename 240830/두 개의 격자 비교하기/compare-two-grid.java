a84fcc7cbeaad8ceddf20c3fad7417848f9b758a
KNG9824 <aktlwjd98@gmail.com>
2024-08-30 07:08:22 +0900

    Title: 두 개의 격자 비교하기, Time: 164ms, Memory: 10MB, Status: Passed - CodeTree

--- 240830/두 개의 격자 비교하기/compare-two-grid.java
+++ 240830/두 개의 격자 비교하기/compare-two-grid.java
@@ -21,9 +21,9 @@ public class Main {
             System.out.print("0 ");
             else
             System.out.print("1 ");
-            System.out.println();
+            
         }
-        
+        System.out.println();
     }
     }
 }
