0b5f82d1ae557ad68809f3cd2a27fefe5b1fe2d8
KNG9824 <aktlwjd98@gmail.com>
2024-08-30 07:05:38 +0900

    Title: 두 배열의 곱, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 240830/두 배열의 곱/multiple-of-two-arrays.java
+++ 240830/두 배열의 곱/multiple-of-two-arrays.java
@@ -4,15 +4,19 @@ public class Main {
         // 여기에 코드를 작성해주세요.
     Scanner sc=new Scanner(System.in);
     int[][] a=new int[3][3];
+    int[][] b=new int[3][3];
     int cnt=1;
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++)
-        System.out.print(cnt+" ");
-        System.out.println();
+        a[i][j]=sc.nextInt();
     }
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++)
-        System.out.print((cnt+1)+" ");
+        b[i][j]=sc.nextInt();
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++)
+        System.out.print((a[i][j]*b[i][j])+" ");
         System.out.println();
     }
     }
