9cb1ff541b07ab851f8ab4c98260d485de474d6d
KNG9824 <aktlwjd98@gmail.com>
2024-09-01 17:49:25 +0900

    Title: 격자 반대로 채우기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240901/격자 반대로 채우기/grid-anti-fill.java
+++ 240901/격자 반대로 채우기/grid-anti-fill.java
@@ -4,25 +4,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] arr = new int[n][n];
-        
+        int[][] matrix = new int[n][n];
+
         int num = 1;
         for (int col = n - 1; col >= 0; col--) {
-            if (col % 2 == 0) { // 짝수 열일 때 (오른쪽에서 왼쪽으로 위로 올라감)
-                for (int row = 0; row < n; row++) {
-                    arr[row][col] = num++;
-                }
-            } else { // 홀수 열일 때 (왼쪽에서 오른쪽으로 아래로 내려감)
+            if ((n - col) % 2 == 1) { // 홀수 번째 열 (맨 마지막 열에서부터 홀수 번째 열일 때)
                 for (int row = n - 1; row >= 0; row--) {
-                    arr[row][col] = num++;
+                    matrix[row][col] = num++;
+                }
+            } else { // 짝수 번째 열
+                for (int row = 0; row < n; row++) {
+                    matrix[row][col] = num++;
                 }
             }
         }
-        
-        // 결과 출력
+
+        // 출력
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(arr[i][j]+" ");
+                System.out.print(matrix[i][j]+" ");
             }
             System.out.println();
         }
